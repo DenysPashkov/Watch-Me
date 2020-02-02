0125f9e3a96bf5c010644c84ea85fd45f8648a5e
denys <denyspashkov@yahoo.com>
2020-02-02 13:37:23 +0100

    Initial Commit

---
+++ Enterprise-WatchMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Enterprise-WatchMe
+//
+//  Created by denys on 02/02/2020.
+//  Copyright © 2020 denys. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
